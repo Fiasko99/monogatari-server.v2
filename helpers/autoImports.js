64597ac6fea88e93782ad981f7dc554edf55bcba
Fiasko <snakeice2000@mail.ru>
2024-04-21 01:06:01 +0300

    add jsdoc for for implicit helper functionality

--- helpers/autoImports.js
+++ helpers/autoImports.js
@@ -1,5 +1,20 @@
 const fs = require('fs')
-
+/**
+ * @description What is it for
+ * Function for auto-importing modules or objects with modules 
+ * Analyzes folders and creates object keys based on folder names
+ * 
+ * @description !!!IMPORTANT!!!
+ * Auto-import forces you to write folder names in camelCase 
+ * if you want to be able to access functions in a convenient way
+ * 
+ * @description !!!IMPORTANT!!!
+ * Auto-import deprives you of the opportunity to go to the target
+ * function since the editor will not understand where you want to go
+ * 
+ * @param {string} path - absolute path to file, should be `__dirname` 
+ * @returns {object} return module or object with modules 
+ */
 module.exports = (path) => {
   const listImports = {}
   fs.readdirSync(path, { withFileTypes: true }).forEach(dir => {
